0afa7c5a221556faf9d84f1a5fdaec3b064931b3
Masenko2604 <138580178+Masenko2604@users.noreply.github.com>
2024-01-30 17:29:54 +0100

    contact

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 // Імпорт стилів нормалізації
 import 'modern-normalize';
-import App from './components/App';
-import './index.css';
+import App from './App'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
 
 
 
